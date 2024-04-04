ad7e6bebd19ef4b87292eae83ba63672b1be136b
berfinmentes <mentesberfin@gmail.com>
2024-04-04 13:38:33 +0300

    LinkedHashMap-Set

---
+++ src/week04/collections/map/linkedhashmap/LinkedHashMpExample01.java
@@ -0,0 +1,19 @@
+package week04.collections.map.linkedhashmap;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMpExample01 {
+    public static void main(String[] args) {
+        LinkedHashMap<String,String> nameMap = new LinkedHashMap<>();
+        nameMap.put("book","kitap");
+        nameMap.put("application","uygulama");
+        nameMap.put("apple","elma");
+        nameMap.put(null, null);
+        nameMap.put(null,"a");
+        nameMap.put("null",null);
+      //  nameMap.put(null,"b");   /*Önceki null değerini siler*/
+        System.out.println("\nLinkedHashMap : "+nameMap);
+
+
+    }
+}
